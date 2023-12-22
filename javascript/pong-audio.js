e536ba0f229f87045a34f75021d0dad9bb05a03a
vec2U <154294309+vec2U@users.noreply.github.com>
2023-12-21 19:01:28 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -60,7 +60,7 @@ ambientSound.player.volume.value = -20; //turn down volume
 export var adventureMusic = new soundFile("silence.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = -5;
 
 export var villageMusic = new soundFile("silence.mp3");
 soundArray.push(villageMusic);

ce2c2bfea9cbd07e14b272912426bde4ab9ab643
vec2U <154294309+vec2U@users.noreply.github.com>
2024-01-03 21:10:19 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -1,4 +1,4 @@
-/*This File creates the sound file players and their default properties
+lo/*This File creates the sound file players and their default properties
 If you create a new player, be sure to import it at the top of index.js!
 */
 
@@ -43,10 +43,10 @@ export var soundArray = []; //list of sounds loaded
 
 //Here is where all the Sound File Players Start
 
-export var wallSound = new soundFile("wall.mp3"); //load sound
+export var wallSound = new soundFile("explosionWall.flac"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("paddle.mp3");
+export var paddleSound = new soundFile("explosionPaddle.wav");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");

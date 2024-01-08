8f43b77c9d49563fa877cd9c93f554bb8eaaaaac
vec2U <154294309+vec2U@users.noreply.github.com>
2024-01-08 03:24:24 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("bigwallexplosion.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("paddleexplosion.mp3");
+export var paddleSound = new soundFile("paddleexplosion2.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("silence.mp3");
